847438e6e686d2b7440903c8342541788467c9d8
BenP1236691 <benjamin20230724@gmail.com>
2024-05-10 22:12:34 +0800

    whatever i did was wrong

--- handleacc.js
+++ handleacc.js
@@ -14,8 +14,8 @@ const firebaseConfig = {
   };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+initializeApp(firebaseConfig);
+getAuth(app);
 
 document.addEventListener('DOMContentLoaded', function () {
     var gloginButton = document.getElementById('googleLoginButton');
